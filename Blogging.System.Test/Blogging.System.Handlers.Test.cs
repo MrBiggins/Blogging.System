1adb715140fd8826990acae7473a2ebb90926699
Deniss Timcenko <denisstim@youforce.net>
2025-09-13 14:35:53 +0100

    Add new test class and update Dockerfile for testing
    Introduces a new test class `Test1` in `Blogging.System.Handlers.Test.cs` with a placeholder method for future implementation. Removes the old `Test1` class from `Test1.cs` to streamline the test structure. Updates the `Dockerfile` to include commands for running tests during the build process, ensuring tests are executed as part of the Docker build. Defines build and publish stages using .NET SDK and ASP.NET runtime images.

---
+++ Blogging.System.Test/Blogging.System.Handlers.Test.cs
@@ -0,0 +1,14 @@
+﻿namespace Blogging.System.Test {
+    [TestClass]
+    public sealed class Test1 {
+        [TestMethod]
+        public void PostHandlerReturnCorrectResulIsValid() {
+
+            //Assign
+
+            //Act
+
+            //Assert
+        }
+    }
+}
